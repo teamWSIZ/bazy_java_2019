734f02ccb76b70debbfa61827125c9ec45a382d4
pmarecki <pmarecki@gmail.com>
2019-06-22 13:34:18 +0200

    x

--- src/main/java/wsi/controller/ScoreController.java
+++ src/main/java/wsi/controller/ScoreController.java
@@ -1,24 +1,39 @@
 package wsi.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
-import wsi.model.*;
-import wsi.service.*;
+import wsi.model.Score;
+import wsi.service.ScoreRepo;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 @RestController
 @CrossOrigin
+@Slf4j
 public class ScoreController {
     @Autowired ScoreRepo scoreRepo;
 
 
     //Messages
     @GetMapping(value = "/scores")
-    public Iterable<Score> getScores() {
-        return scoreRepo.findAllByOrderByScoreDesc();
+    public Iterable<Score> getScores(
+            @RequestParam(value = "limit", defaultValue = "10000") Integer limit,
+            @RequestParam(value = "alias") String alias
+    ) {
+        log.info("Setting limit: {}", limit);
+        Pageable p = PageRequest.of(0, limit);
+
+        if (alias==null) {
+            ///....
+        } else {
+            ////
+        }
+        //todo: pole "since" typu Data.......
+
+        return scoreRepo.findAllByOrderByScoreDesc(p);
     }
 
     @PostMapping(value = "/scores")
--- src/main/java/wsi/service/ScoreRepo.java
+++ src/main/java/wsi/service/ScoreRepo.java
@@ -1,9 +1,15 @@
 package wsi.service;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import wsi.model.Customer;
 import wsi.model.Score;
 
+import java.util.List;
+
 public interface ScoreRepo extends CrudRepository<Score,Integer> {
-    Iterable<Score> findAllByOrderByScoreDesc();
+
+    List<Score> findAllByOrderByScoreDesc(Pageable p);
+
+    List<Score> findAllByAliasOrderByScoreDesc(String alias, Pageable p);
 }
